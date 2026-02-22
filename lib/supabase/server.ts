c09fe797c3a9932ebd8030642d037fe94650740d
yehtet <yhtet1934@gmail.com>
2026-02-22 12:59:14 +0800

    change the supabase api key name

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,8 +3,7 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY ?? process.env.SUPABASE_SERVICE_ROLE_KEY;
-
+const supabaseSecretKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY ?? process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export async function createSupabaseServerClient() {
   if (!supabaseUrl || !supabaseAnonKey) {
